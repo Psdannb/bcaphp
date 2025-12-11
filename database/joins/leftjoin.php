8f8038e09e71d99736770cebcbc34447643fe90d
Psdannb <psdannb@gmail.com>
2025-12-11 13:50:01 +0545

    left join in sql

---
+++ database/joins/leftjoin.php
@@ -0,0 +1,37 @@
+<?php
+// Returns all rows from the left table and matching rows from the right table.
+// Non-matching rows from the right table are shown as NULL.
+// here i have used two tables customers with fields(cid,fullname,city)
+// and orders with fields (oid,product,orderdate)
+require "../db.php";
+$sql="SELECT * FROM customers LEFT JOIN orders ON customers.cid=orders.cid";
+$response= mysqli_query($connection, $sql);
+// print_r($response);
+
+?>
+<table border="1">
+    <tr>
+        <th>full name</th>
+        <th>city</th>
+        <th> order product</th>
+        <th>order date</th>
+    </tr>
+
+    <?php
+foreach($response as $data){
+    // print_r($data);
+    // die();
+    ?>
+    <tr>
+        <td><?php echo $data['fullname'] ?></td>
+        <td><?php echo $data['city'] ?></td>
+        <td><?php echo $data['product'] ?></td>
+        <td><?php echo $data['orderdate'] ?></td>
+    </tr>
+    <?php
+}
+?>
+</table>
+<?php
+mysqli_close($connection);
+?>
